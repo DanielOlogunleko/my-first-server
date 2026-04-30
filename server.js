da41fe62e16bd172c763b87f1b099ca63090a8b3
DanielOlogunleko <Daniel.Ologunleko91@myhunter.cuny.edu>
2026-04-30 00:04:17 -0400

    created a server

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-// Your server code goes here. Host your server on localhost:3000
+// Your server code goes here. Host your server on localhost:3000// 
+
+console.log('Node is running');
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error loading index.html');
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(3002, () => {
+  console.log('Server running at http://localhost:3002');
+});
